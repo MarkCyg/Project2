dad0b1753604fdc984d521ca7e9262431a49c82c
MarkCyg <87244359+MarkCyg@users.noreply.github.com>
2021-10-24 11:52:29 -0400

    added modals

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+//SIGN IN MODAL
+var modal = document.getElementById("siModal");
+
+var btn = document.getElementById("signin");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//SIGN UP MODAL
+var modal = document.getElementById("suModal");
+
+var btn1 = document.getElementById("signup");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn1.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
