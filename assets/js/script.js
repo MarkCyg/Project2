a7f18a53a304071e5e739eb74944069251f550e0
MarkCyg <87244359+MarkCyg@users.noreply.github.com>
2021-10-24 12:51:19 -0400

    modal build

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,48 @@
 //SIGN IN MODAL
-var modal = document.getElementById("siModal");
+// Get the modal
+var modal = document.getElementById('myModal');
 
-var btn = document.getElementById("signin");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
+// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+// When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+    modal.style.display = "block";
 }
 
+// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
     modal.style.display = "none";
-  }
 }
 
-//SIGN UP MODAL
-var modal = document.getElementById("suModal");
+// Get the modal
+var modal2 = document.getElementById('myModal2');
 
-var btn1 = document.getElementById("signup");
+// Get the button that opens the modal
+var btn2 = document.getElementById("myBtn2");
 
-var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close2")[0];
 
-btn1.onclick = function() {
-  modal.style.display = "block";
+// When the user clicks the button, open the modal 
+btn2.onclick = function() {
+    modal2.style.display = "block";
 }
 
-span.onclick = function() {
-  modal.style.display = "none";
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+    modal2.style.display = "none";
 }
 
+// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-      modal.style.display = "none";
+        modal.style.display = "none";
     }
-  }
+    else if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
