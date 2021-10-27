d2766da107de77d8e6cc6780e5e4ca0e6ed47a08
Ting's Mac <huxu@uwplatt.edu>
2021-10-27 13:37:19 -0500

    Updated category-seeds

---
+++ seeds/category-seeds.js
@@ -0,0 +1,20 @@
+const { Category } = require("../models");
+
+//Can have multiple categories for a post
+const categoryData = [
+  {
+    category_name: "HTML",
+  },
+  {
+    category_name: "git",
+  },
+  {
+    category_name: "git",
+  },
+  {
+    category_name: "Other",
+  },
+  {
+    category_name: "mySQL",
+  },
+];
