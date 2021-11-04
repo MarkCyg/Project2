9fb3baa5b801dfb63c6468bac349bbd9b6a9dbc6
Ting's Mac <huxu@uwplatt.edu>
2021-11-04 01:30:39 -0500

    Located the reason for login/logout, need implementation for home-route.js in the controllers folder. Not enough time to update... :(

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,24 +1,58 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Category, Comment, Post, PostTag, Tag, User } = require("../models");
 
-router.get('/', (req, res) => {
-    res.render('home');
-})
+// router.get("/", (req, res) => {
+//   res.render("home");
+// });
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('login');
-  });
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: ["id", "post_text", "user_id", "created_at"],
+    order: [["created_at", "DESC"]],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+      res.render("home", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
 
 // router.get('/login', (req, res) => {
 //     res.render('login');
 // })
 
-router.get('/home', (req, res) => {
-    res.render('home');
-})
+router.get("/home", (req, res) => {
+  res.render("home");
+});
 module.exports = router;
