ea83460b4bd05517c0c942559e2b37079f100d61
Ting's Mac <huxu@uwplatt.edu>
2021-11-03 23:39:47 -0500

    Added more handbars

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,48 +1,60 @@
 async function loginFormHandler(event) {
   event.preventDefault();
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
   if (email && password) {
-      const response = await fetch('/api/users/login', {
-          method: 'post',
-          body: JSON.stringify({
-              email,
-              password
-          }),
-          headers: { 'Content-Type': 'application/json' }
-      });
-      if (response.ok) {
-          if (document.location === 'http://project2.herokuapp.com/login' || 'localhost:3001/login') {
-              document.location.replace('/home');
-          } else {
-              alert(response.statusText);
-          }
+    const response = await fetch("/api/users/login", {
+      method: "post",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      if (
+        document.location === "http://project2.herokuapp.com/login" ||
+        document.location === "localhost:3001/login"
+      ) {
+        document.location.replace("/home");
+      } else {
+        alert(response.statusText);
       }
+    }
   }
 }
+
 async function signupFormHandler(event) {
   event.preventDefault();
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
   if (email && password && username) {
-      const response = await fetch('/api/users', {
-          method: 'post',
-          body: JSON.stringify({
-              email,
-              password,
-              username
-          }),
-          headers: { 'Content-Type': 'application/json' }
-      });
-      if (response.ok) {
-          if (document.location === 'http://project2.herokuapp.com/login' || 'localhost:3001/login') {
-              document.location.replace('/home');
-          } else {
-              alert(response.statusText);
-          }
+    const response = await fetch("/api/users", {
+      method: "post",
+      body: JSON.stringify({
+        email,
+        password,
+        username,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      if (
+        document.location === "http://project2.herokuapp.com/login" ||
+        "localhost:3001/login"
+      ) {
+        document.location.replace("/home");
+      } else {
+        alert(response.statusText);
       }
+    }
   }
 }
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
+const helpers = require("./utils/helpers");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-const session = require('express-session');
-const { timeEnd } = require('console');
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+const session = require("express-session");
+const { timeEnd } = require("console");
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 //set up session
 const sess = {
@@ -26,14 +26,14 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 app.use(session(sess));
 
 // turn on routes
 app.use(routes);
-app.use(require('./controllers/'));
+app.use(require("./controllers/"));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
