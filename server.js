8bd7715ca92714d07a90cc11a1b21826006b2eff
MarkCyg <87244359+MarkCyg@users.noreply.github.com>
2021-11-09 12:37:51 -0500

    why

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ app.use(require("./controllers/")
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(Pprocess.env.PORT || 3000, () => console.log("Now listening"));
+  app.listen(process.env.PORT || 3000, () => console.log("Now listening"));
 });
